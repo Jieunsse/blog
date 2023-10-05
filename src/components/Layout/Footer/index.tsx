d37f565c4b779425d4257bafd8cc76425268c876
jieunsse <jieunsse@gmail.com>
2023-10-05 22:36:20 +0900

    Layout files are sorted to each folders

---
+++ src/components/Layout/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrapper } from './style.ts';
+
+const Footer = () => {
+    return(
+        <>
+            <Wrapper>
+                <footer>
+                    <h2>This will be footer.</h2>
+                </footer>
+            </Wrapper>
+        </>
+    );
+}
+
+
+
+export default Footer;
